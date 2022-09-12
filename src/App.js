08e2f1e3ee8ff1c760eede57622695313c785096
Katrina <kalynndier@gmail.com>
2022-09-12 11:58:15 -0400

    register and login

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import TopNav from './components/nav/TopNav';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './view/home/Home';
 import Dashboard from './view/dashboard/Dashboard';
+import Register from './view/register/Register';
+import Login from './view/login/Login';
 
 function App() {
   return (
-    <div>
-    <BrowserRouter>
-    <TopNav />
-    <section className='App-header '>
-        <img src={'https://blog.hertimetocode.com/wp-content/uploads/2022/08/HELP.png'} 
-        alt = "Help App logo. Help with heart in background."
-        height="150px"
-        />
-      </section>
-        <Routes>
-          <Route exact path='/' element={<Home />}/>
-          <Route path='/dashboard' element={<Dashboard />}/>
-        </Routes>
-      </BrowserRouter>
-      </div>
+    <>
+    <div className='App-header '>
+    <img src={'https://blog.hertimetocode.com/wp-content/uploads/2022/08/HELP.png'} 
+    alt = "Help App logo. Help with heart in background."
+    height="150px"
+    />
+    </div>
+    <Register />
+    </>
     );
   }
   
   export default App;
+
+
+  // <main>
+  //  <BrowserRouter>
+
+  //  <Routes>
+  //  <Route exact path='/' element={<Home />}/>
+  //  <Route path='/dashboard' element={<Dashboard />}/>
+  //  </Routes>
+  //  </BrowserRouter>
+  //  </main>
   
   
   // <section className = 'App'>
